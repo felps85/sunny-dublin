5f2b0c225401cc6ddaaac3edd18f0eb0c8e9b8e5
Felipe Soares <lfcosta.r@gmail.com>
2026-03-27 11:53:46 +0000

    Add GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/sunny-dublin/" : "/",
   plugins: [react()],
   server: {
     port: 5173
   }
-});
-
+}));
